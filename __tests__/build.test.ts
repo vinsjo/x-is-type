5a4dba2220522cd32c00eaaf7d834ea46cc3dc3f
Vincent Sjögren <vincent.sjogren@bitlog.se>
2025-07-11 18:52:56 +0200

    did some cleaning up in tests

--- __tests__/build.test.ts
+++ __tests__/build.test.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import * as x from '..';
 
 import { runTests } from './runTests';
--- __tests__/runTests.ts
+++ __tests__/runTests.ts
@@ -1,90 +1,141 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { describe, expect, test } from '@jest/globals';
-import type * as src from '../src';
 
-type TestArgs = [expectedFalseArgs: unknown[][], expectedTrueArgs: unknown[][]];
+/**
+ * Type of the exports from the source module.
+ */
+type Source = typeof import('../src');
 
-const args = (first: unknown, ...rest: unknown[]): unknown[][] =>
-    [first, ...rest].map((arg) => [arg]);
+type TestFunctionValues = [
+    // Values where function is expected to return false
+    expectedFalseValues: unknown[],
+    // Values where function is expected to return true
+    expectedTrueValues: unknown[]
+];
 
-const createRunTests = (tests: { [K in keyof typeof src]: TestArgs }) => {
-    const testCallbacks = (
-        Object.entries(tests) as Array<[name: keyof typeof src, args: TestArgs]>
-    ).map(([name, [expectedFalseArgs, expectedTrueArgs]]) => {
-        const onUnexpected = (args: unknown[], result: boolean) => {
-            console.log(
-                `unexpected result: ${name}(${args
-                    .map((arg) => String(arg))
-                    .join(', ')}) === ${result}`
-            );
-        };
-        return (x: typeof src) => {
-            const typeChecker = x[name] as (...args: unknown[]) => boolean;
+/**
+ * Function names exported from the source module.
+ */
+type ExportedFunctionName = Extract<keyof Source, string>;
 
-            describe(name, () => {
-                test('expect false', () => {
-                    for (const args of expectedFalseArgs) {
-                        const result = typeChecker(...args);
-                        if (result) onUnexpected(args, result);
-                        expect(result).toBe(false);
-                    }
-                });
-                test('expect true', () => {
-                    for (const args of expectedTrueArgs) {
-                        const result = typeChecker(...args);
-                        if (!result) onUnexpected(args, result);
-                        expect(result).toBe(true);
-                    }
-                });
-            });
-        };
-    });
+const testValueToString = (value: unknown): string => {
+    if (value == null || typeof value !== 'object') {
+        return typeof value === 'string' ? `"${value}"` : String(value);
+    }
+    if (
+        Array.isArray(value) ||
+        Object.getPrototypeOf(value) === Object.prototype
+    ) {
+        return JSON.stringify(value);
+    }
+    if (value instanceof Date) {
+        return Number.isNaN(value.getTime()) ? 'Invalid Date' : 'Date';
+    }
 
-    return (x: typeof src) => {
-        testCallbacks.forEach((test) => test(x));
-    };
+    if (value instanceof Set || value instanceof Map) {
+        return `${value instanceof Set ? 'Set' : 'Map'}(${JSON.stringify([
+            ...value,
+        ])})`;
+    }
+
+    return value.toString();
 };
 
-export const runTests = createRunTests({
-    isNum: [args('', NaN), args(1, 100, Infinity)],
-    isEmptyObj: [
-        args(null, '', Symbol(), { foo: 'bar' }, [1]),
-        args({}, [], new Set(), new Map()),
+const runFunctionTest = (
+    source: Source,
+    name: ExportedFunctionName,
+    [expectedFalseValues, expectedTrueValues]: TestFunctionValues
+) => {
+    const fn = source[name] as (value: unknown) => boolean;
+
+    const defineTests = (values: unknown[], expectedResult: boolean) => {
+        for (const value of values) {
+            test(`expect ${name}(${testValueToString(
+                value
+            )}) to return ${expectedResult}`, () => {
+                expect(fn(value)).toBe(expectedResult);
+            });
+        }
+    };
+
+    describe(name, () => {
+        defineTests(expectedFalseValues, false);
+        defineTests(expectedTrueValues, true);
+    });
+};
+
+const argsByName: Record<ExportedFunctionName, TestFunctionValues> = {
+    isNum: [
+        ['', NaN, new Number()],
+        [1, 100, Infinity],
     ],
-    isStr: [args(1, null, {}), args('', String())],
-    isBool: [args(1, null, {}), args(true, false, Boolean())],
-    isObj: [args(null, '', Symbol(1)), args({}, [], new Date())],
-    isArr: [args({}, new Set(), new Map()), args([], Array(1))],
+    isStr: [
+        [1, null, {}, new String()],
+        ['', String()],
+    ],
+    isBool: [
+        [1, null, {}],
+        [true, false, Boolean()],
+    ],
+    isObj: [
+        [null, '', Symbol(1)],
+        [{}, [], new Date()],
+    ],
+    isArr: [
+        [{}, new Set(), new Map()],
+        [[], Array(1)],
+    ],
     isFn: [
-        args(null, {}, 1),
-        args(
-            () => {},
-            function () {}
-        ),
+        [null, {}, 1],
+        [() => {}, function () {}, class TestClass {}],
     ],
-    isNull: [args(undefined, false, 0, {}), args(null)],
-    isUndef: [args(null, 0, false, NaN), args(undefined)],
-    isNullish: [args(false, 0, 'undefined', NaN), args(null, undefined)],
-    isDate: [args({}, new Date().toString(), 0), args(new Date())],
-    isSymbol: [args({}, [], '', 1), args(Symbol())],
-    isMap: [args([], {}, new Set()), args(new Map())],
-    isSet: [args([], {}, new Map()), args(new Set())],
-    isInt: [args(1.1, NaN, Infinity), args(1, 2, 3)],
-    isFloat: [args(1, NaN, Infinity), args(1.1, 3.14, 0.0000000001)],
+    isNull: [[undefined, false, 0, {}], [null]],
+    isUndef: [[null, 0, false, NaN], [undefined]],
+    isNullish: [
+        [false, 0, 'undefined', NaN],
+        [null, undefined],
+    ],
+    isDate: [[{}, new Date().toString(), 0], [new Date()]],
+    isSymbol: [[{}, [], '', 1], [Symbol()]],
+    isMap: [[[], {}, new Set()], [new Map()]],
+    isSet: [[[], {}, new Map()], [new Set()]],
+    isInt: [
+        [1.1, NaN, Infinity],
+        [1, 2, 3],
+    ],
+    isFloat: [
+        [1, NaN, Infinity],
+        [1.1, 3.14, 0.0000000001],
+    ],
     isValidDate: [
-        args({}, new Date().toString(), new Date('invalid')),
-        args(new Date(), new Date('1970-01-01')),
+        [{}, new Date().toString(), new Date('invalid')],
+        [new Date(), new Date('1970-01-01')],
     ],
     isFalsy: [
-        args(1, ' ', true, [], {}),
-        args(null, undefined, false, 0, '', NaN),
+        [1, ' ', true, [], {}],
+        [null, undefined, false, 0, '', NaN],
     ],
     isTruthy: [
-        args(null, undefined, false, 0, '', NaN),
-        args(1, ' ', true, [], {}),
+        [null, undefined, false, 0, '', NaN],
+        [1, ' ', true, [], {}],
+    ],
+    isEmptyObj: [
+        [null, '', Symbol(), { foo: 'bar' }, [1]],
+        [{}, [], new Set(), new Map()],
     ],
     isRegularObj: [
-        args(null, '', Symbol(1), [], new Set(), new Map()),
-        args({}, { foo: 'bar' }, { a: 1, b: 2 }, { x: 'y' }),
+        [null, '', Symbol(1), [], new Set(), new Map()],
+        [{}, { foo: 'bar' }, { a: 1, b: 2 }, { x: 'y' }],
     ],
-});
+};
+
+/**
+ * Run tests for the exported functions.
+ *
+ * @param source - Exported functions from `src` or `dist`
+ */
+export const runTests = async (source: Source) => {
+    (Object.keys(argsByName) as (keyof typeof argsByName)[]).forEach((name) => {
+        runFunctionTest(source, name, argsByName[name]);
+    });
+};
